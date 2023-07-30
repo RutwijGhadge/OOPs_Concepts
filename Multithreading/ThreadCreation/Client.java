187b193db5d7a437c308a6d473d76daf77eaca5f
RutwijGhadge <80887753+RutwijGhadge@users.noreply.github.com>
2023-07-30 09:48:03 +0530

    Update Client.java

--- Multithreading/ThreadCreation/Client.java
+++ Multithreading/ThreadCreation/Client.java
@@ -1,1 +1,16 @@
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+
+        for(int i=1;i<=x;i++){
+            TableCreator t= new TableCreator(x);
+            ScalerThread t1= new ScalerThread(t);
+            t1.start();
+        }
+
+    }
+}
 
