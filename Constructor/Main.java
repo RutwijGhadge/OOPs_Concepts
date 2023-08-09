8650e436b76eaee722b6fad3f012af9539d4895a
RutwijGhadge <80887753+RutwijGhadge@users.noreply.github.com>
2023-08-09 12:10:18 +0530

    Add files via upload

---
+++ Constructor/Main.java
@@ -0,0 +1,29 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Car C=new Car(900,5,10);
+        Car p=new Car(C);        //Deep Copy
+        Car p=C;            //Shallow Copy
+
+        /*When we do a copy of some entity to create two or more than two entities
+        such that changes in one entity are reflected in the other entities as well,
+        then we can say we have done a shallow copy
+
+        Car p object holds the address of Car C object
+        Changes done in attribute of object C will be reflected in attributes of P
+        as they both are pointing to the same memory address
+         */
+
+        /*When we do a copy of some entity to create two or more than two entities
+         such that changes in one entity are not reflected in the other entities,
+         then we can say we have done a deep copy.
+
+         deep copy -> two separate copies
+
+         line no 7
+         */
+
+    }
+}
