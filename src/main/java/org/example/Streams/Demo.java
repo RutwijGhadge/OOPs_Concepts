4f21331b55fa33dfcc5baf9f9a72ea93b33c5c04
RutwijGhadge <rutwij0507.rg@gmail.com>
2026-01-09 09:26:31 +0530

    "Added Streams Examples"

--- src/main/java/org/example/Streams/Demo.java
+++ src/main/java/org/example/Streams/Demo.java
@@ -1,6 +1,7 @@
 package org.example.Streams;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collector;
@@ -38,6 +39,80 @@ public class Demo {
         Explanation: The map function applies String::toUpperCase to each element,
         transforming them to uppercase.
          */
-        
+
+
+        List < Integer > nums = Arrays.asList(1, 3, 6, 8, 10, 18, 36);
+        double Average=nums.stream()
+                .mapToDouble(Integer::doubleValue)
+                .average()
+                .orElse(0.0);
+        System.out.println("Average:"+ Average);
+
+        List < String > colors = Arrays.asList("RED", "grEEn", "white", "Orange", "pink");
+        List<String>Upper=colors.stream()
+                .map(String::toUpperCase)
+                .toList();
+        System.out.println("UpperCase:"+ Upper);
+
+        List<String>Lower=colors.stream()
+                .map(String::toLowerCase)
+                .toList();
+        System.out.println(Lower);
+
+        List < Integer > number = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        int even_sum=number.stream()
+                .filter(s->s%2==0)
+                .mapToInt(Integer::intValue)
+                .sum();
+        System.out.println("Sum:"+even_sum);
+
+        int odd_sum=number.stream()
+                .filter(s->s%2!=0)
+                .mapToInt(Integer::intValue)
+                .sum();
+        System.out.println("Sum:"+odd_sum);
+
+        List < Integer > num = Arrays.asList(10, 23, 22, 23, 24, 24, 33, 15, 26, 15);
+        List<Integer>distinct=num.stream()
+                .distinct()
+                .toList();
+        System.out.println(distinct);
+
+        List < String > color = Arrays.asList("Red", "Green", "Blue", "Pink", "Brown");
+        char startingChar='B';
+        long count=color.stream()
+                .filter(s->s.startsWith(String.valueOf(startingChar)))
+                .count();
+        System.out.println("Number of String Starting with given Starting character:"+count);
+
+        List<String>ascendingOrder=color.stream()
+                .sorted()
+                .toList();
+        System.out.println("Ascending order Sorting:"+ascendingOrder);
+
+        List<String>descendingOrder=color.stream()
+                .sorted(Comparator.reverseOrder())
+                .toList();
+        System.out.println("Descending Order Sorting:"+descendingOrder);
+
+        List < Integer > numb = Arrays.asList(1, 17, 54, 14, 14, 33, 45, -11);
+        Integer max_value=numb.stream()
+                .max(Integer::compare)
+                .orElse(null);
+        System.out.println("Maximum Value:"+max_value);
+
+        Integer min_value=numb.stream()
+                .min(Integer::compare)
+                .orElse(null);
+        System.out.println("Minimum Value:"+min_value);
+
+        Integer second_smallest=numb.stream()
+                .distinct()
+                .sorted()
+                .skip(1)
+                .findFirst()
+                .orElse(null);
+        System.out.println("Second Smallest:"+second_smallest);
+
     }
 }
